6a655ec6da36221785a8afa481a81b571a37d5c8
Olga <krohaleva@rambler.ru>
2018-11-27 14:23:33 +0700

    Логи

--- Application/src/main/java/com/example/RxCameraBasic/rxcamera2/CameraRxWrapper.kt
+++ Application/src/main/java/com/example/RxCameraBasic/rxcamera2/CameraRxWrapper.kt
@@ -2,17 +2,10 @@ package com.example.RxCameraBasic.rxcamera2
 
 import android.annotation.SuppressLint
 import android.annotation.TargetApi
-import android.hardware.camera2.CameraCaptureSession
-import android.hardware.camera2.CameraDevice
-import android.hardware.camera2.CameraManager
-import android.hardware.camera2.CaptureFailure
-import android.hardware.camera2.CaptureRequest
-import android.hardware.camera2.CaptureResult
-import android.hardware.camera2.TotalCaptureResult
-import android.util.Log
+import android.hardware.camera2.*
 import android.util.Pair
 import android.view.Surface
-
+import com.example.RxCameraBasic.MainActivity
 import io.reactivex.Observable
 import io.reactivex.ObservableEmitter
 
@@ -26,8 +19,9 @@ object CameraRxWrapper {
     private val TAG = CameraRxWrapper::class.java.name
 
     private fun log(msg: String) {
-        Log.d(TAG, msg)
+        MainActivity.log("$TAG: $msg")
     }
+
     //-----------------------------------------------------------------------------------------------
     /**
      * @see CameraDevice.StateCallback
@@ -47,37 +41,40 @@ object CameraRxWrapper {
 
         return Observable.create { camEmitter ->
 
-            log("\topenCamera")
+            log("openCamera")
 
-            camEmitter.setCancellable { log("\topenCamera - unsubscribed") }
+            camEmitter.setCancellable { log("openCamera - unsubscribed") }
 
             cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
-                override fun onOpened(cameraDevice: CameraDevice) {
-                    log("\topenCamera - onOpened")
+
+                override fun onOpened(camera: CameraDevice) {
+                    log("camera ${camera.id}: openCamera - onOpened")
 
                     if (!camEmitter.isDisposed) {
-                        camEmitter.onNext(Pair(DeviceStateEvents.ON_OPENED, cameraDevice))
+                        camEmitter.onNext(Pair(DeviceStateEvents.ON_OPENED, camera))
                     }
                 }
 
-                override fun onClosed(cameraDevice: CameraDevice) {
-                    log("\topenCamera - onClosed")
+                override fun onClosed(camera: CameraDevice) {
+                    log("camera ${camera.id}: openCamera - onClosed")
 
                     if (!camEmitter.isDisposed) {
-                        camEmitter.onNext(Pair(DeviceStateEvents.ON_CLOSED, cameraDevice))
+                        camEmitter.onNext(Pair(DeviceStateEvents.ON_CLOSED, camera))
                         camEmitter.onComplete()
                     }
                 }
 
-                override fun onDisconnected(cameraDevice: CameraDevice) {
+                override fun onDisconnected(camera: CameraDevice) {
+                    log("camera ${camera.id}: openCamera - onDisconnected")
+
                     if (!camEmitter.isDisposed) {
-                        camEmitter.onNext(Pair(DeviceStateEvents.ON_DISCONNECTED, cameraDevice))
+                        camEmitter.onNext(Pair(DeviceStateEvents.ON_DISCONNECTED, camera))
                         camEmitter.onComplete()
                     }
                 }
 
                 override fun onError(camera: CameraDevice, error: Int) {
-                    log("\topenCamera - onError")
+                    log("camera ${camera.id}: openCamera - onError")
 
                     if (!camEmitter.isDisposed) {
                         camEmitter.onError(OpenCameraException(OpenCameraException.Reason.getReason(error)))
@@ -99,20 +96,20 @@ object CameraRxWrapper {
     }
     //-----------------------------------------------------------------------------------------------
     /*
-     Создание сессии для указанного устройства cameraDevice.
+     Создание сессии для указанного устройства camera.
      surfaceList - список Surface, которые будут использованы для записи данных с устройства.
      */
-    fun createCaptureSession(cameraDevice: CameraDevice, surfaceList: List<Surface>): Observable<Pair<CaptureSessionStateEvents, CameraCaptureSession>> {
+    fun createCaptureSession(camera: CameraDevice, surfaceList: List<Surface>): Observable<Pair<CaptureSessionStateEvents, CameraCaptureSession>> {
         return Observable.create { sesEmitter ->
 
-            log("\tcreateCaptureSession")
+            log("camera ${camera.id}: createCaptureSession")
 
-            sesEmitter.setCancellable { log("\tcreateCaptureSession - unsubscribed") }
+            sesEmitter.setCancellable { log("camera ${camera.id}: createCaptureSession - unsubscribed") }
 
-            cameraDevice.createCaptureSession(surfaceList, object : CameraCaptureSession.StateCallback() {
+            camera.createCaptureSession(surfaceList, object : CameraCaptureSession.StateCallback() {
 
                 override fun onConfigured(session: CameraCaptureSession) {
-                    log("\tcreateCaptureSession - onConfigured")
+                    log("camera ${session.device.id}: createCaptureSession - onConfigured")
 
                     if (!sesEmitter.isDisposed) {
                         sesEmitter.onNext(Pair(CaptureSessionStateEvents.ON_CONFIGURED, session))
@@ -120,7 +117,7 @@ object CameraRxWrapper {
                 }
 
                 override fun onConfigureFailed(session: CameraCaptureSession) {
-                    log("\tcreateCaptureSession - onConfigureFailed")
+                    log("camera ${session.device.id}: createCaptureSession - onConfigureFailed")
 
                     if (!sesEmitter.isDisposed) {
                         sesEmitter.onError(CreateCaptureSessionException(session))
@@ -128,7 +125,7 @@ object CameraRxWrapper {
                 }
 
                 override fun onReady(session: CameraCaptureSession) {
-                    log("\tcreateCaptureSession - onReady")
+                    log("camera ${session.device.id}: createCaptureSession - onReady")
 
                     if (!sesEmitter.isDisposed) {
                         sesEmitter.onNext(Pair(CaptureSessionStateEvents.ON_READY, session))
@@ -136,7 +133,7 @@ object CameraRxWrapper {
                 }
 
                 override fun onActive(session: CameraCaptureSession) {
-                    log("\tcreateCaptureSession - onActive")
+                    log("camera ${session.device.id}: createCaptureSession - onActive")
 
                     if (!sesEmitter.isDisposed) {
                         sesEmitter.onNext(Pair(CaptureSessionStateEvents.ON_ACTIVE, session))
@@ -144,7 +141,7 @@ object CameraRxWrapper {
                 }
 
                 override fun onClosed(session: CameraCaptureSession) {
-                    log("\tcreateCaptureSession - onClosed")
+                    log("camera ${session.device.id}: createCaptureSession - onClosed")
 
                     if (!sesEmitter.isDisposed) {
                         sesEmitter.onNext(Pair(CaptureSessionStateEvents.ON_CLOSED, session))
@@ -153,7 +150,7 @@ object CameraRxWrapper {
                 }
 
                 override fun onSurfacePrepared(session: CameraCaptureSession, surface: Surface) {
-                    log("\tcreateCaptureSession - onSurfacePrepared")
+                    log("camera ${session.device.id}: createCaptureSession - onSurfacePrepared")
 
                     if (!sesEmitter.isDisposed) {
                         sesEmitter.onNext(Pair(CaptureSessionStateEvents.ON_SURFACE_PREPARED, session))
